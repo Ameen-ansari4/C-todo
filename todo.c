3134e78ff36268dea51e4a265615db84dfa92fc8
Ameen Ansari <mohdameenansari9@gmail.com>
2025-10-28 08:06:12 +0530

    Add show method

--- todo.c
+++ todo.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 int main(int argc,char* argv[]){
 
+	FILE* fp;
+	char buff[100];
+	int c;
 
+// Check argument count if less than 2
+	if(argc<2){
+		printf("Error: argument required.\n");
+		exit(1);
+	}
+
+/*
+// Open .tasks file for operations
+	fp = fopen(".tasks", "ar");
+	if(fp == NULL){
+		printf("can't access tasks\n");
+		exit(1);
+	}
+
+*/
+
+//show todo list, read from .tasks
+	if(strcmp(argv[1], "show") == 0){
+		fp = fopen(".tasks", "r");
+		if(fp != NULL){
+			while((c=fgetc(fp)) != EOF){
+				printf("%c", c);
+			}
+		}
+		else{
+			printf("Error: Cant't access tasks\n");
+			exit(1);
+		}
+		fclose(fp);
+	}
 	return 0;
 }
