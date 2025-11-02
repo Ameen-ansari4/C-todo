c65d22e8bebdff629d0649e4e91e9f8b922763c3
Ameen Ansari <mohdameenansari9@gmail.com>
2025-11-02 09:56:49 +0530

    Add delete feature

--- todo.c
+++ todo.c
@@ -5,7 +5,7 @@
 
 int main(int argc,char* argv[]){
 
-	FILE* fp;
+	FILE* fp, *tmp;
 	char buff[100];
 	int c;
 
@@ -56,7 +56,50 @@ int main(int argc,char* argv[]){
 		}
 		fclose(fp);
 	}
+//Delete tasks from the file
+	else if(strcmp(argv[1],"delete") == 0){
+		if(argc<3){
+			printf("argument required <task>\n");
+			exit(1);
+		}
 
+		int line = atoi(argv[2]);
+		int count =0;
+
+		if(line<=0 || line>100){
+			printf("Error: Invalid task ID.\n");
+			exit(1);
+		}
+
+
+		//open both files
+		fp = fopen(".tasks", "r");
+		tmp = fopen("tmp","w");
+
+		if(fp == NULL || tmp == NULL){
+			printf("couldn't delete task, Error occured\n");
+			exit(1);
+		}
+
+		//read and write in files
+		while(fgets(buff,100,fp) !=NULL){
+			count++;
+			if(count == line)
+				continue;
+			fputs(buff, tmp);
+		}
+
+		fclose(fp);
+		fclose(tmp);
+
+		// rename tmo file and remove original .takss file
+		remove(".tasks");
+		rename("tmp", ".tasks");
+
+		if(count == 0)
+		printf("No taks to delete!\n");
+		else printf("One task deleted!\n");
+	}
 
 	return 0;
 }
